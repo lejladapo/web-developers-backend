9411593d698949c854d72315debf6e92808b7d92
lejladapo <lejla.djapo@stu.ssst.edu.ba>
2022-04-22 12:55:59 +0200

    Initial commit

---
+++ src/main/java/com/webdevelopersbackend/WebDevelopersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webdevelopersbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDevelopersBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDevelopersBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webdevelopersbackend/WebDevelopersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdevelopersbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDevelopersBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
