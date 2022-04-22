e713d7b6800d2a28ac8d73cf68c2c063fff72b2a
lejladapo <102324557+lejladapo@users.noreply.github.com>
2022-04-22 14:36:40 +0200

    Added: delete mapping for deleting a project (#2)

--- src/main/java/com/webdevelopersbackend/controllers/ProjectController.java
+++ src/main/java/com/webdevelopersbackend/controllers/ProjectController.java
@@ -2,10 +2,7 @@ package com.webdevelopersbackend.controllers;
 
 import com.webdevelopersbackend.models.Project;
 import com.webdevelopersbackend.services.ProjectService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -21,5 +18,14 @@ public class ProjectController {
     @GetMapping("/{id}")
     public Project getOneProject(@PathVariable long id) {
         return this.projectService.getOneProject(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteProject(@PathVariable long id) {
+        Project project = projectService.deleteProject(id);
+        if(project == null) {
+            throw new RuntimeException("Id does not exist");
+        }
+        return "Project was deleted.";
     }
 }
