d0e5283f14cff8cda9ead85c542b6536551c2c0d
Ismar-Becirspahic <ismar.becirspahic@stu.ssst.edu.ba>
2022-05-23 23:03:59 +0200

    Added: unit testing for DevProfile service

--- src/main/java/com/webdevelopersbackend/services/DevProfileService.java
+++ src/main/java/com/webdevelopersbackend/services/DevProfileService.java
@@ -4,31 +4,16 @@ import com.webdevelopersbackend.models.DevProfile;
 import com.webdevelopersbackend.repositories.DevProfileRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class DevProfileService {
 
-    private final List<DevProfile> listDevProfile = new ArrayList<>();
     private final DevProfileRepository devProfileRepository;
 
     public DevProfileService(DevProfileRepository devProfileRepository) {
         this.devProfileRepository = devProfileRepository;
-        listDevProfile.add(testProfile());
-    }
-
-    private DevProfile testProfile() {
-        DevProfile developer = new DevProfile();
-        developer.setId(1);
-        developer.setName("Ismar");
-        developer.setSurname("Becirspahic");
-        developer.setLocation("Germany");
-        developer.setNumberOfProjectsCompleted(3);
-        developer.setRecentCompletedProject("Something");
-        developer.setYearsOfExperience(1);
-        return developer;
     }
 
     public DevProfile getProfile(long id) {
@@ -49,17 +34,8 @@ public class DevProfileService {
         return devProfileRepository.save(devProfile);
     }
     public DevProfile updateProfile(DevProfile devProfile, long id){
-        for (DevProfile currentDevProfile: listDevProfile){
-            if (currentDevProfile.getId() == id){
-                currentDevProfile.setName(devProfile.getName());
-                currentDevProfile.setSurname(devProfile.getSurname());
-                currentDevProfile.setLocation(devProfile.getLocation());
-                currentDevProfile.setYearsOfExperience(devProfile.getYearsOfExperience());
-                currentDevProfile.setNumberOfProjectsCompleted(devProfile.getNumberOfProjectsCompleted());
-                currentDevProfile.setRecentCompletedProject(devProfile.getRecentCompletedProject());
-
-            }
-        }
+        getProfile(id);
+        devProfile.setId(id);
         return devProfileRepository.save(devProfile);
     }
 }
---
+++ src/test/java/com/webdevelopersbackend/services/ProfileInforServiceTest.java
@@ -0,0 +1,150 @@
+package com.webdevelopersbackend.services;
+import com.webdevelopersbackend.data.ProfileInforTest;
+import com.webdevelopersbackend.models.DevProfile;
+import com.webdevelopersbackend.repositories.DevProfileRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(SpringRunner.class)
+public class ProfileInforServiceTest {
+
+    @MockBean
+    private DevProfileRepository profileRepository;
+
+    @TestConfiguration
+    static class ItemServiceTestContextConfiguration {
+        @Bean
+        @Primary
+        public DevProfileService profileService(DevProfileRepository profileRepository) {
+            return new DevProfileService(profileRepository);
+        }
+    }
+    @Autowired
+    private DevProfileService profileService;
+
+    @Test
+    public void givenProfiles_whenGettingProfiles_thenProfilesShouldBeFound() {
+        Mockito.when(profileRepository.findAll())
+                .thenReturn(List.of(ProfileInforTest.profile()));
+
+        List<DevProfile> returnedList = profileService.getListDevProfile();
+
+        assertThat(returnedList).hasSize(1);
+    }
+
+    @Test
+    public void givenNoProfiles_whenGettingProfiles_thenListShouldBeEmpty() {
+        assertThat(profileService.getListDevProfile().isEmpty());
+    }
+
+    @Test
+    public void givenProfiles_whenGettingProfiles_thenRepositoryIsCalled() {
+        profileService.getListDevProfile();
+
+        verify(profileRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void givenValidId_whenGettingOneProfile_thenProfileShouldBeFound() {
+        DevProfile profile = ProfileInforTest.profile();
+
+        Mockito.when(profileRepository.findById(profile.getId()))
+                .thenReturn(Optional.of(profile));
+
+        DevProfile resultList = profileService.getProfile(profile.getId());
+
+        assertThat(resultList.getName())
+                .isEqualTo(profile.getName());
+    }
+
+    @Test
+    public void givenInvalidId_whenGettingOneProfile_thenExceptionShouldBeThrown() {
+        assertThatThrownBy(() -> profileService.getProfile(14L))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("Id invalid.");
+    }
+
+    @Test
+    public void givenProfile_whenGettingOneProfile_thenProjectIsReturned() {
+        DevProfile input = ProfileInforTest.profile();
+        input.setId(0L);
+        DevProfile output = ProfileInforTest.profile();
+
+        Mockito.when(profileRepository.save(input))
+                .thenReturn(output);
+
+        DevProfile profile = profileService.addProfile(input);
+
+        assertThat(profile).isNotNull();
+        assertThat(profile.getName()).isEqualTo(input.getName());
+    }
+
+    @Test
+    public void givenProfile_whenDeletingProfile_thenRepositoryIsCalled() {
+        long id = ProfileInforTest.profile().getId();
+
+        profileService.deleteProfile(id);
+
+        verify(profileRepository, times(1)).deleteById(id);
+    }
+
+    @Test
+    public void givenProfile_whenAddingProfile_thenRepositoryIsCalled() {
+        DevProfile profile = ProfileInforTest.profile();
+
+        profileService.addProfile(profile);
+
+        verify(profileRepository, times(1)).save(profile);
+    }
+
+    @Test
+    public void givenProfile_whenAddingProfile_thenIdIsAssigned() {
+        DevProfile input = ProfileInforTest.profile();
+        input.setId(0L);
+        DevProfile output = ProfileInforTest.profile();
+
+        Mockito.when(profileRepository.save(input))
+                .thenReturn(output);
+
+        DevProfile result = profileService.addProfile(input);
+
+        assertThat(result.getId()).isNotEqualTo(0L);
+    }
+
+    @Test
+    public void givenProfileWithValidId_whenUpdatingProfile_thenProfileIsReturned() {
+        DevProfile input = ProfileInforTest.profile();
+        input.setId(0L);
+        long id = 2L;
+        DevProfile output = ProfileInforTest.profile();
+        output.setId(id);
+
+        Mockito.when(profileRepository.findById(id))
+                .thenReturn(Optional.of(output));
+        Mockito.when(profileRepository.save(input))
+                .thenReturn(output);
+
+        DevProfile resultItem = profileService.updateProfile(input, id);
+
+        assertThat(resultItem).isNotNull();
+        assertThat(resultItem.getName()).isEqualTo(input.getName());
+        assertThat(resultItem.getId()).isEqualTo(id);
+    }
+
+}
+
